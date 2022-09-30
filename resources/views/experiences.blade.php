92fa79df3e6230677722e0c02310c80a2cf8787b
ricky-yap <ricky.yap@icloud.com>
2022-09-30 23:03:38 +0700

    first commit

--- resources/views/experiences.blade.php
+++ resources/views/experiences.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Experience- Ricky Yap</title>
+    <title>Experiences - Ricky Yap</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
